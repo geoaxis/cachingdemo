94d475ede3fe6690ccfd6e18a301b8820c6a528e
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2021-05-21 14:48:03 +0200

    Creates basic spring boot 2.5 application

---
+++ src/main/java/com/hatimonline/spring/cachingdemo/CachingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.hatimonline.spring.cachingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingdemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CachingdemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hatimonline/spring/cachingdemo/CachingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatimonline.spring.cachingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingdemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
