636a0f4d39ace884353ab8c8589dc3e61f01691d
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2021-05-31 21:02:37 +0200

    Adds elastic logging/APM for local dev
    Elastic stack deployed using https://github.com/geoaxis/local-elastic-stack

--- src/main/java/com/hatimonline/spring/cachingdemo/CachingdemoApplication.java
+++ src/main/java/com/hatimonline/spring/cachingdemo/CachingdemoApplication.java
@@ -1,5 +1,6 @@
 package com.hatimonline.spring.cachingdemo;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class CachingdemoApplication {
 
   public static void main(String[] args) {
+    ElasticApmAttacher.attach();
     SpringApplication.run(CachingdemoApplication.class, args);
   }
 
